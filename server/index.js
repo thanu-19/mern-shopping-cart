63c509f2024ef1598e69a26888d1793e4331c584
thanu-19 <thanu232005@gmail.com>
2025-05-23 10:10:39 +0530

    Added index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: 'https://mern-shopping-cart-one.vercel.app'  // your frontend URL here
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
